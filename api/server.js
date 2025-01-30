0800405300cd9cc2b066acbbdc73d4a9ce0a5c79
Umair Ahmed <umairjdu@gmail.com>
2025-01-30 13:14:30 +0500

    update

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const app = require("../app");
 
-const mongoose = require("mongoose");
 
+/*
 const connectDB = async () => {
   try {
     await mongoose.connect("mongodb+srv://umairjd:netro709@saylanimicrofinance.czqqg.mongodb.net/miami");
@@ -11,7 +11,10 @@ const connectDB = async () => {
   }
 };
 
+*/
+
 app.listen(3000, () => {
-  connectDB();
   console.log("server is running on port 3000");
 });
+
+module.exports = app;
--- app.js
+++ app.js
@@ -1,37 +1,18 @@
 const express = require("express");
-const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 
 // mongoose
-const UserSchema = mongoose.Schema({
-  name: String,
-  age: Number,
-});
 
-const User = mongoose.model("User", UserSchema);
-
-app.get("/home", (req, res) => {
-  res.send("get request");
-});
-
-app.post("/", async (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    age: req.body.age,
-  });
-
-  await user.save();
-
-  res.send("post request");
+app.get("/", (req, res) => {
+  res.send("get request from local vercel");
 });
 
 app.get("/users", async (req, res) => {
   try {
-    const users = await User.find({});
-    res.send({ users, message: "success" });
+    res.send({ body: req.body, message: "success" });
   } catch (error) {
     res.send(error);
   }
